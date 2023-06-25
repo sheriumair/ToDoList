419b670abc5db12ceae09bd8bcc899b76c52d042
HP <HP@DESKTOP-M9HD6A8>
2023-06-25 21:10:19 +0500

    Final Commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+    watch: {
+      usePolling: true,
+    },
+  },
+});
